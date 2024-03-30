d98ab4e6b40ac23bb186134b9b9fa421363dcbce
xAllow <alvaro31032003@gmail.com>
2024-03-30 20:10:36 +0100

    ajustes

--- YadosFitness/src/app/app.component.ts
+++ YadosFitness/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbNavModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { DietasComponent } from './dietas/dietas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbNavModule, NgbDropdownModule], 
+  imports: [RouterOutlet, NgbNavModule, NgbDropdownModule, DietasComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
