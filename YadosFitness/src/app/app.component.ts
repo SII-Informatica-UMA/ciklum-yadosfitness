fefe8df6996cf0190d3b261d8c214eb1d641e238
xAllow <alvaro31032003@gmail.com>
2024-03-31 20:10:26 +0200

    inicio e imagenes

--- YadosFitness/src/app/app.component.ts
+++ YadosFitness/src/app/app.component.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NgbNavModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { DietasComponent } from './dietas/dietas.component';
-
+import { InicioComponent } from './inicio/inicio.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbNavModule, NgbDropdownModule, DietasComponent, CommonModule], 
+  imports: [RouterOutlet, NgbNavModule, NgbDropdownModule, DietasComponent, CommonModule, InicioComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ YadosFitness/src/app/inicio/inicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+@Component({
+  selector: 'app-inicio',
+  standalone: true,
+  imports: [NgbCarouselModule],
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.css'
+})
+export class InicioComponent {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+}
