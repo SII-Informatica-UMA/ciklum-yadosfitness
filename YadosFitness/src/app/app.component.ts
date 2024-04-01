93788baa5fcaeb4ff1b245fab18387d35051d2e1
Jorge_uma <117035917+JorgeCastilloPodadera@users.noreply.github.com>
2024-04-01 21:04:57 +0200

    prueba login

--- YadosFitness/src/app/app.component.ts
+++ YadosFitness/src/app/app.component.ts
@@ -1,19 +1,59 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule, TitleCasePipe } from '@angular/common';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
 import { NgbNavModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { DietasComponent } from './dietas/dietas.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { UsuariosService } from './services/usuarios.service';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbNavModule, NgbDropdownModule, DietasComponent, CommonModule, InicioComponent], 
+  imports: [RouterOutlet, NgbNavModule, NgbDropdownModule, DietasComponent, CommonModule, InicioComponent, RouterLink, FormsModule, TitleCasePipe], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'YadosFitness';
   active = 1;
+  _rolIndex: number = 0;
+
+  constructor(private usuarioService: UsuariosService, private router: Router) {
+    this.actualizarRol()
+  }
+
+  get rolIndex() {
+    return this._rolIndex;
+  }
+
+  set rolIndex(i: number) {
+    this._rolIndex = i;
+    this.actualizarRol();
+  }
+
+  actualizarRol() {
+    let u = this.usuarioSesion;
+    if (u) {
+      this.usuarioService.rolCentro = u.roles[this.rolIndex];
+    } else {
+      this.usuarioService.rolCentro = undefined;
+    }
+  }
+
+  get rol() {
+    return this.usuarioService.rolCentro;
+  }
+
+  get usuarioSesion() {
+    return this.usuarioService.getUsuarioSesion();
+  }
+
+  logout() {
+    this.usuarioService.doLogout();
+    this.actualizarRol();
+    this.router.navigateByUrl('/login');
+  }
 }
 
 
