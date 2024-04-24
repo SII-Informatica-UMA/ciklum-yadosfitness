08ede2990c96ad549d354d31123eae59096d9c02
xAllow <alvluqtoruma@uma.es>
2024-04-24 14:59:13 +0200

    correcion

--- Backend/entidad/src/main/java/YadosFitness/entidad/repositories/DietaRepository.java
+++ Backend/entidad/src/main/java/YadosFitness/entidad/repositories/DietaRepository.java
@@ -11,16 +11,15 @@ import org.springframework.data.repository.query.Param;
 import YadosFitness.entidad.entities.Dieta;
 
 public interface DietaRepository extends JpaRepository<Dieta, Long> {
-
-    @Query( "Insert into Dieta(nombre, descripcion, observaciones, objetivos, duracionDias, alimentos, recomendaciones, entrenadorId, clienteId) values(:nombre, :descripcion, :observaciones, :objetivos, :duracionDias, :alimentos, :recomendaciones, :entrenadorId, :clienteId)" )
+    @Query("INSERT INTO Dieta(nombre, descripcion, observaciones, objetivo, duracionDias, alimentos, recomendaciones, entrenadorId, clienteId) VALUES (:nombre, :descripcion, :observaciones, :objetivo, :duracionDias, :alimentos, :recomendaciones, :entrenadorId, :clienteId)")
     void insertDieta(@Param("nombre") String nombre, @Param("descripcion") String descripcion, @Param("observaciones") String observaciones, @Param("objetivo") String objetivo, @Param("duracionDias") int duracionDias, @Param("alimentos") ArrayList<String> alimentos, @Param("recomendaciones") String recomendaciones, @Param("entrenadorId") Long entrenadorId, @Param("clienteId") Set<Long> clienteId);
-    @Query( "Update Dieta Set nombre=:nombre, descripcion=:descripcion,observaciones=:observaciones,duracionDias=:duracionDias,alimentos=:alimentos,recomendaciones=:recomendaciones,clienteId=:clienteId Where id=:id AND entrenadorId:=entrenadorId")
+    @Query("UPDATE Dieta SET nombre = :nombre, descripcion = :descripcion, observaciones = :observaciones, objetivo = :objetivo, duracionDias = :duracionDias, alimentos = :alimentos, recomendaciones = :recomendaciones, clienteId = :clienteId WHERE id = :id AND entrenadorId = :entrenadorId")
     void updateDieta(@Param("nombre") String nombre, @Param("descripcion") String descripcion, @Param("observaciones") String observaciones, @Param("objetivo") String objetivo, @Param("duracionDias") int duracionDias, @Param("alimentos") ArrayList<String> alimentos, @Param("recomendaciones") String recomendaciones, @Param("entrenadorId") Long entrenadorId, @Param("clienteId") Set<Long> clienteId,@Param("id") Long id);
-    @Query( "Update Dieta Set clienteId=:clienteId where id=:id AND entrenadorId:=entrenadorId")
+    @Query("UPDATE Dieta SET clienteId = :clienteId WHERE id = :id AND entrenadorId = :entrenadorId")
     void updateCliente(@Param("entrenadorId") Long entrenadorId, @Param("clienteId") Set<Long> clienteId,@Param("id") Long id);
     
-    List<Dieta> findByCliente(Set<Long> cliente);
-    List<Dieta> findByEntrenador(Long entrenador);
+    List<Dieta> findByClienteId(Set<Long> clienteId);
+    List<Dieta> findByEntrenadorId(Long entrenadorid);
     List<Dieta> findByNombre(String nombre);
     
 }
